7f48235847d6976ec68dd2afea5815955b2be712
Revanth Kanna M <revanthkannam05@gmail.com>
2026-04-05 10:57:48 +0530

    favicon changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     icon: [
       { url: "/icon-light-32x32.png", media: "(prefers-color-scheme: light)" },
       { url: "/icon-dark-32x32.png", media: "(prefers-color-scheme: dark)" },
-      { url: "/icon.svg", type: "image/svg+xml" },
+      { url: "/icon.svg", type: "image/svg" },
     ],
     apple: "/apple-icon.png",
   },
