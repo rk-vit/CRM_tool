e4f926bf1a1fc61d368025d82e43caa6c69116fd
Revanth Kanna M <revanthkannam05@gmail.com>
2026-07-13 20:19:42 +0530

    Fallback to local logging for dev env

--- instrumentation.ts
+++ instrumentation.ts
@@ -8,30 +8,50 @@ declare global {
 
 export async function register() {
   // 1. Register OpenTelemetry (keep this!)
-  registerOTel({
-    serviceName: "crm-app",
-  });
+  const useLocalTelemetry =
+    process.env.LOCAL_LOGGING === "true" || // or create LOCAL_OTEL
+    !process.env.OTEL_EXPORTER_OTLP_ENDPOINT;
+
+  if (!useLocalTelemetry) {
+    registerOTel({
+      serviceName: "crm-app",
+    });
+  }
+
 
   // 2. Only initialize Pino on the Node runtime
   if (process.env.NEXT_RUNTIME !== "nodejs") return;
 
   const pino = (await import("pino")).default;
-  const pinoLoki = (await import("pino-loki")).default;
+  const useLocalLogging =
+    process.env.LOCAL_LOGGING === "true" ||
+    !process.env.GRAFANA_LOKI_HOST ||
+    !process.env.GRAFANA_LOKI_USERNAME ||
+    !process.env.GRAFANA_LOKI_PASSWORD;
 
-  const stream = pinoLoki({
-    host: process.env.GRAFANA_LOKI_HOST!,
-    basicAuth: {
-      username: process.env.GRAFANA_LOKI_USERNAME!,
-      password: process.env.GRAFANA_LOKI_PASSWORD!,
-    },
-    batching: {
-      interval: 5,
-    },
-    labels: {
-      app: "crm",
-      environment: process.env.NODE_ENV,
-    },
-  });
+  const stream = useLocalLogging
+    ? pino.transport({
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "SYS:standard",
+          singleLine: false,
+        },
+      })
+    : (await import("pino-loki")).default({
+        host: process.env.GRAFANA_LOKI_HOST!,
+        basicAuth: {
+          username: process.env.GRAFANA_LOKI_USERNAME!,
+          password: process.env.GRAFANA_LOKI_PASSWORD!,
+        },
+        batching: {
+          interval: 5,
+        },
+        labels: {
+          app: "crm",
+          environment: process.env.NODE_ENV,
+        },
+      });
 
   globalThis.logger = pino(
     {
